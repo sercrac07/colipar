f6876e6ed7e9f3297d47aebe87810994052ef425
Sercrac07 <sercassan@gmail.com>
2024-08-22 01:45:36 +0200

    feat: add main function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-export default class Cliuc {}
+import { argv } from "node:process"
+import { getFlag } from "./lib/utils"
+
+interface CliucOptions<Flags extends Flag<string>> {
+  flags: Flags
+}
+
+type Flag<T extends string> = {
+  [key in T]: FlagOptions
+}
+
+interface FlagOptions {
+  type: "string" | "boolean"
+  short?: string
+}
+
+export default function cliuc<T extends string, Flags extends Flag<T>>(
+  options: CliucOptions<Flags>
+): {
+  [Key in keyof typeof options.flags]: (typeof options.flags)[Key]["type"] extends "string" ? string : (typeof options.flags)[Key]["type"] extends "boolean" ? boolean : never
+} {
+  const args = argv.slice(2)
+
+  const obj = {} as Record<string, string | boolean | undefined>
+  Object.keys(options.flags).forEach(key => {
+    if (options.flags[key as T].type === "boolean") {
+      obj[key] = getFlag(`--${key}`, "boolean", args)
+      if (options.flags[key as T].short) obj[key] = getFlag(`-${options.flags[key as T].short}`, "boolean", args)
+    } else {
+      obj[key] = getFlag(`--${key}`, "string", args)
+      if (options.flags[key as T].short) obj[key] = getFlag(`-${options.flags[key as T].short}`, "string", args)
+    }
+  })
+  return obj as {
+    [Key in keyof typeof options.flags]: (typeof options.flags)[Key]["type"] extends "string" ? string : (typeof options.flags)[Key]["type"] extends "boolean" ? boolean : never
+  }
+}
---
+++ src/lib/utils.ts
@@ -0,0 +1,34 @@
+export function getFlag<T extends "string" | "boolean">(flag: string, type: T, argv: string[]): T extends "string" ? string | undefined : boolean {
+  // Find the position of the flag in the argv array
+  const flagIndex = argv.findIndex(arg => arg.startsWith(`${flag}=`) || arg === flag)
+
+  if (type === "boolean") {
+    if (flagIndex !== -1) {
+      const value = argv[flagIndex].split("=")[1]
+      if (typeof value === "undefined") {
+        return true as T extends "string" ? string | undefined : boolean // Solo la flag sin valor, interpretamos como true
+      }
+      // Interpret specific values as true/false
+      return (value === "true") as T extends "string" ? string | undefined : boolean
+    } else {
+      return false as T extends "string" ? string | undefined : boolean
+    }
+  } else if (type === "string") {
+    if (flagIndex !== -1) {
+      const [, value] = argv[flagIndex].split("=")
+      if (typeof value !== "undefined") {
+        // Return the value of the flag
+        return value.replace(/^["']|["']$/g, "") as T extends "string" ? string | undefined : boolean
+      } else if (flagIndex + 1 < argv.length) {
+        // Return the next argument if no explicit value is assigned
+        return argv[flagIndex + 1] as T extends "string" ? string | undefined : boolean
+      } else {
+        return "" as T extends "string" ? string | undefined : boolean
+      }
+    } else {
+      return undefined as T extends "string" ? string | undefined : boolean
+    }
+  }
+
+  return undefined as T extends "string" ? string | undefined : boolean
+}
